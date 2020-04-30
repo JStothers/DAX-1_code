b85f2d502c1bac92aa24fe1b155ea1c7f3ce82e1
JStothers <40527369+JStothers@users.noreply.github.com>
2020-04-30 14:38:10 -0700

    Add files via upload

---
+++ other/Expression_Matrix.R
@@ -0,0 +1,34 @@
+setwd("/Users/jstot/Desktop/Jess_work/Currently_Trying")
+# https://bioconductor.org/packages/devel/bioc/vignettes/TCGAbiolinks/inst/doc/analysis.html#tcgaanalyze:_analyze_data_from_tcga
+
+library(TCGAbiolinks)
+library(SummarizedExperiment)
+
+# You can define a list of samples to query and download providing relative TCGA barcodes.
+listSamples <- c("TCGA-E9-A1NG-11A-52R-A14M-07","TCGA-BH-A1FC-11A-32R-A13Q-07",
+                 "TCGA-A7-A13G-11A-51R-A13Q-07","TCGA-BH-A0DK-11A-13R-A089-07",
+                 "TCGA-E9-A1RH-11A-34R-A169-07","TCGA-BH-A0AU-01A-11R-A12P-07",
+                 "TCGA-C8-A1HJ-01A-11R-A13Q-07","TCGA-A7-A13D-01A-13R-A12P-07",
+                 "TCGA-A2-A0CV-01A-31R-A115-07","TCGA-AQ-A0Y5-01A-11R-A14M-07")
+
+# Query platform Illumina HiSeq with a list of barcode 
+query <- GDCquery(project = "TCGA-BRCA", 
+                  data.category = "Gene expression",
+                  data.type = "Gene expression quantification",
+                  experimental.strategy = "RNA-Seq",
+                  platform = "Illumina HiSeq",
+                  file.type = "results",
+                  barcode = listSamples, 
+                  legacy = TRUE)
+
+# Download a list of barcodes with platform IlluminaHiSeq_RNASeqV2
+GDCdownload(query)
+
+# Prepare expression matrix with geneID in the rows and samples (barcode) in the columns
+# rsem.genes.results as values
+BRCARnaseqSE <- GDCprepare(query)
+
+BRCAMatrix <- assay(BRCARnaseqSE,"raw_count") # or BRCAMatrix <- assay(BRCARnaseqSE,"raw_count")
+
+# For gene expression if you need to see a boxplot correlation and AAIC plot to define outliers you can run
+BRCARnaseq_CorOutliers <- TCGAanalyze_Preprocessing(BRCARnaseqSE)
